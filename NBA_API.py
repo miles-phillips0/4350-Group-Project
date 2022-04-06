9bba7ed717f34603e97f42b6cd982d70e245cc52
KevinRodgersGSU <krodgers9@student.gsu.edu>
2022-04-06 00:43:33 -0400

    Added API functionality to be able to search from a date to another date

--- NBA_API.py
+++ NBA_API.py
@@ -1,14 +1,28 @@
 from nba_api.stats.endpoints import commonplayerinfo
 from nba_api.stats.static import players
+from nba_api.stats.library.parameters import SeasonAll
+from nba_api.stats.endpoints import playergamelog
 
 player_info = commonplayerinfo.CommonPlayerInfo(player_id=2544)
 playerdict = player_info.player_headline_stats.get_dict()
 print(playerdict)
 print(playerdict["data"][0][0])
-Players = players.find_players_by_full_name("James Anderson")
+Players = players.find_players_by_full_name("Lebron James")
 test = Players[0]
-print(Players[0])
+print(Players)
 print(test["id"])
+
+gamelog_bron_all = playergamelog.PlayerGameLog(player_id="2544")
+df_bron_games_all = gamelog_bron_all.get_dict()
+print(df_bron_games_all["resultSets"][0]["rowSet"][0])
+gamelog_bron_all = playergamelog.PlayerGameLog(player_id="2544", season=SeasonAll.all)
+df_bron_games_all = gamelog_bron_all.get_dict()
+##print(df_bron_games_all)
+gamelog_bron_all = playergamelog.PlayerGameLog(
+    player_id="2544", date_from_nullable="12/25/2020", date_to_nullable="12/25/2021"
+)
+df_bron_games_all = gamelog_bron_all.get_dict()
+print(df_bron_games_all)
 
 
 def get_player_id(name):
