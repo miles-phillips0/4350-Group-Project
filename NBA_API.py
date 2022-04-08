a6bc8f5cd96f358cbca4433c965f0735a979c7ed
KevinRodgersGSU <krodgers9@student.gsu.edu>
2022-04-08 19:51:46 -0400

    fixed pylint errors

--- NBA_API.py
+++ NBA_API.py
@@ -1,36 +1,8 @@
+"""File with functions to handle nba_api calls"""
 from nba_api.stats.endpoints import commonplayerinfo
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import playergamelog
 import pandas as pd
-
-""""
-Meant for testing code will delete later
-player_info = commonplayerinfo.CommonPlayerInfo(player_id=2544)
-playerdict = player_info.player_headline_stats.get_dict()
-print(playerdict)
-print(playerdict["data"][0][0])
-Players = players.find_players_by_full_name("Lebron James")
-test = Players[0]
-print(Players)
-print(test["id"])
-gamelog_bron_all = playergamelog.PlayerGameLog(player_id="2544")
-df_bron_games_all = gamelog_bron_all.get_dict()
-##print(df_bron_games_all["resultSets"][0]["rowSet"][0])
-gamelog_bron_all = playergamelog.PlayerGameLog(player_id="2544", season=SeasonAll.all)
-df_bron_games_all = gamelog_bron_all.get_dict()
-##print(df_bron_games_all)
-gamelog_bron_all = playergamelog.PlayerGameLog(
-    player_id="2544", date_from_nullable="12/25/2020", date_to_nullable="12/25/2021"
-)
-df_bron_games_all = gamelog_bron_all.get_data_frames()
-df = pd.DataFrame(df_bron_games_all[0])
-df2 = df["PTS"].mean()
-##print(df_bron_games_all)
-df.plot(x="AST", y="PTS", kind="scatter")
-print(plt.get_backend())
-plt.show(block=True)
-print(df2)
-"""
 
 
 def get_player_id(name):
@@ -58,20 +30,18 @@ def get_player_info(player_id):
 
 
 def get_player_games_between_dates(date_from, date_to, player_id):
-    """Function to return panda dataframe with info from all games a player has played from and to specific dates"""
+    """Function to return panda dataframe of all games a player has played between dates"""
     if None in (date_from, date_to, player_id):
         return None
     gamelog_all = playergamelog.PlayerGameLog(
         player_id=player_id, date_from_nullable=date_from, date_to_nullable=date_to
     )
+    print(gamelog_all)
     games_all = gamelog_all.get_data_frames()
-    df = pd.DataFrame(games_all[0])
-    if df.empty:
+    pd_df = pd.DataFrame(games_all[0])
+    if pd_df.empty:
         return "No games for this player during time"
-    return df
+    return pd_df
 
 
-"""
-print(get_player_id("ngmjskfdngfjksa"))
-print(get_player_games_between_dates("12/25/2021", "12/25/2020", "2544"))
-"""
+get_player_games_between_dates("12/25/2021", "12/25/2020", "2544")
