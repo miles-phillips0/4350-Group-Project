f1194cda4f993494d30b0be2f0054fcaaccfdc1b
Miles <miles@DESKTOP-CEDMDNJ.localdomain>
2022-04-08 18:53:25 -0400

    added player search

--- NBA_API.py
+++ NBA_API.py
@@ -3,45 +3,12 @@ from nba_api.stats.static import players
 from nba_api.stats.library.parameters import SeasonAll
 from nba_api.stats.endpoints import playergamelog
 import pandas as pd
-import matplotlib.pyplot as plt
-
-""""
-Meant for testing code will delete later
-
-player_info = commonplayerinfo.CommonPlayerInfo(player_id=2544)
-playerdict = player_info.player_headline_stats.get_dict()
-print(playerdict)
-print(playerdict["data"][0][0])
-Players = players.find_players_by_full_name("Lebron James")
-test = Players[0]
-print(Players)
-print(test["id"])
-
-gamelog_bron_all = playergamelog.PlayerGameLog(player_id="2544")
-df_bron_games_all = gamelog_bron_all.get_dict()
-##print(df_bron_games_all["resultSets"][0]["rowSet"][0])
-gamelog_bron_all = playergamelog.PlayerGameLog(player_id="2544", season=SeasonAll.all)
-df_bron_games_all = gamelog_bron_all.get_dict()
-##print(df_bron_games_all)
-gamelog_bron_all = playergamelog.PlayerGameLog(
-    player_id="2544", date_from_nullable="12/25/2020", date_to_nullable="12/25/2021"
-)
-df_bron_games_all = gamelog_bron_all.get_data_frames()
-df = pd.DataFrame(df_bron_games_all[0])
-df2 = df["PTS"].mean()
-
-##print(df_bron_games_all)
-df.plot(x="AST", y="PTS", kind="scatter")
-print(plt.get_backend())
-plt.show(block=True)
-print(df2)
-"""
 
 
 def get_player_id(name):
     playersList = players.find_players_by_full_name(name)
     player = playersList[0]
-    return player["id"]
+    return playersList[0]["id"]
 
 
 def get_player_info(id):
@@ -58,7 +25,7 @@ def get_player_info(id):
 
 def get_player_games_between_dates(date_from, date_to, id):
     gamelog_all = playergamelog.PlayerGameLog(
-        player_id=id, date_from_nullable=date_from, date_to_nullable=date_to
+        player_id=str(id), date_from_nullable=date_from, date_to_nullable=date_to
     )
     games_all = gamelog_all.get_data_frames()
     df = pd.DataFrame(games_all[0])
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask_login import (
     login_user,
     LoginManager,
+    UserMixin,
     login_required,
     logout_user,
     current_user,
@@ -11,7 +12,9 @@ from flask_sqlalchemy import SQLAlchemy
 import flask
 from dotenv import load_dotenv, find_dotenv
 import bcrypt
-from sqlalchemy.dialects.postgresql import BYTEA
+from sqlalchemy.dialects.postgresql import BYTEA, ARRAY
+from NBA_API import get_player_id, get_player_info, get_player_games_between_dates
+import pandas as pd
 
 # Loading .env Postgres DB & Secret Keys
 load_dotenv(find_dotenv())
@@ -34,10 +37,15 @@ db = SQLAlchemy(app)
 # Created table for Users to be added to
 
 
-class User(db.Model):
+class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), nullable=False)
     hash = db.Column(BYTEA, nullable=False)
+    roster = db.Column(db.String(120), default="")
+
+    @property
+    def getRoster(self):
+        return [int(x) for x in self.roster.split(";")]
 
 
 db.create_all()
@@ -50,7 +58,7 @@ login_manager.login_view = "signin"
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return Users.query.get(int(user_id))
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -66,15 +74,50 @@ def index():
             flash("Password Not Entered")
             return flask.render_template("login.html")
 
-        if not User.query.filter_by(email=email).first():
+        if not Users.query.filter_by(email=email).first():
             hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-            newUser = User(email=email, hash=hashed)
+            newUser = Users(email=email, hash=hashed, roster="")
             db.session.add(newUser)
             db.session.commit()
             return flask.render_template("login.html")
         flash(f"{email} is already registered")
 
     return flask.render_template("login.html")
+
+
+@app.route("/add", methods=["GET", "POST"])
+@login_required
+def addPlayer():
+    return 0
+
+
+@app.route("/home", methods=["GET", "POST"])
+@login_required
+def home():
+    if flask.request.method == "POST":
+        data = flask.request.form
+        playerName = data["playerSearch"]
+        playerID = str(get_player_id(playerName))
+        playerGamelog = get_player_games_between_dates(
+            "12/25/2020", "12/25/2021", playerID
+        )
+        averagePoints = round(playerGamelog["PTS"].mean(), 2)
+        averageRebounds = round(playerGamelog["REB"].mean(), 2)
+        averageAssists = round(playerGamelog["AST"].mean(), 2)
+        len_results = 1
+        print(playerGamelog)
+        print("POST")
+        return flask.render_template(
+            "index.html",
+            len_results=len_results,
+            playerName=playerName,
+            averageAssists=averageAssists,
+            averagePoints=averagePoints,
+            averageRebounds=averageRebounds,
+        )
+    print("GET")
+
+    return flask.render_template("index.html", len_results=0)
 
 
 @app.route("/signin", methods=["GET", "POST"])
@@ -91,11 +134,12 @@ def signin():
         if password == "":
             flash("Password Not Entered")
             return flask.render_template("login.html")
-        user = User.query.filter_by(email=email).first()
+        user = Users.query.filter_by(email=email).first()
 
         if user:
             if bcrypt.checkpw(password.encode("utf-8"), user.hash):
-                return flask.render_template("index.html")
+                login_user(user)
+                return flask.redirect("/home")
             flash(f"Incorrect Password for {email}")
             return flask.render_template("login.html")
 
