b317abcacae83559950a09d7c8b39f666bef07aa
KevinRodgersGSU <krodgers9@student.gsu.edu>
2022-04-08 06:12:24 -0400

    Turned testing code to comments and made function return panda data frame instead of dict

--- NBA_API.py
+++ NBA_API.py
@@ -2,8 +2,11 @@ from nba_api.stats.endpoints import commonplayerinfo
 from nba_api.stats.static import players
 from nba_api.stats.library.parameters import SeasonAll
 from nba_api.stats.endpoints import playergamelog
-import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+""""
+Meant for testing code will delete later
 
 player_info = commonplayerinfo.CommonPlayerInfo(player_id=2544)
 playerdict = player_info.player_headline_stats.get_dict()
@@ -16,7 +19,7 @@ print(test["id"])
 
 gamelog_bron_all = playergamelog.PlayerGameLog(player_id="2544")
 df_bron_games_all = gamelog_bron_all.get_dict()
-print(df_bron_games_all["resultSets"][0]["rowSet"][0])
+##print(df_bron_games_all["resultSets"][0]["rowSet"][0])
 gamelog_bron_all = playergamelog.PlayerGameLog(player_id="2544", season=SeasonAll.all)
 df_bron_games_all = gamelog_bron_all.get_dict()
 ##print(df_bron_games_all)
@@ -27,8 +30,12 @@ df_bron_games_all = gamelog_bron_all.get_data_frames()
 df = pd.DataFrame(df_bron_games_all[0])
 df2 = df["PTS"].mean()
 
-print(df_bron_games_all)
+##print(df_bron_games_all)
+df.plot(x="AST", y="PTS", kind="scatter")
+print(plt.get_backend())
+plt.show(block=True)
 print(df2)
+"""
 
 
 def get_player_id(name):
@@ -53,5 +60,6 @@ def get_player_games_between_dates(date_from, date_to, id):
     gamelog_all = playergamelog.PlayerGameLog(
         player_id=id, date_from_nullable=date_from, date_to_nullable=date_to
     )
-    games_all = gamelog_all.get_dict()
-    return games_all
+    games_all = gamelog_all.get_data_frames()
+    df = pd.DataFrame(games_all[0])
+    return df
